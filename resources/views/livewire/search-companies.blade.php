2a0442e03aa240e07014f2bbc3b34af5f19d574e
Nelson-5553 <andresnelson2004@hotmail.com>
2025-09-04 14:09:16 -0500

    Add delete modal component and integrate into search companies view

--- resources/views/livewire/search-companies.blade.php
+++ resources/views/livewire/search-companies.blade.php
@@ -45,10 +45,11 @@
                                         class="bg-[#D946EF] text-white text-xs font-medium me-2 px-2.5 py-1 rounded-sm">{{ $empresa->estado }}</span>
                                 @endif
                             </td>
-                            <td class="px-6 py-4">
+                            <td class="flex flex-row px-6 py-4 gap-2">
                                 <a href="{{ route('empresas.edit', $empresa) }}"
                                     class="text-blue-500"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg></a>
-                            </td>
+                            <x-delete-modal />
+                                </td>
                         </tr>
                     @endforeach
                 @endif
