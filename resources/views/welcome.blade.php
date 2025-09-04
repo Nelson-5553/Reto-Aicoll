02718d6bf07d0b91b052cbd5c34260c6facf3154
Nelson-5553 <andresnelson2004@hotmail.com>
2025-09-03 21:23:57 -0500

    Add CreateCompanies component and integrate it into the welcome page with Flowbite support

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,13 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
   </head>
   <body >
     <x-nav-bar />
     <div class="max-w-4xl mx-auto p-4">
       <x-card-state />
+      <x-create-companies />
       <x-companies-table />
     </div>
   </body>
