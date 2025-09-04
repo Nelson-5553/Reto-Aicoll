ba9a45df36fcf4da8ae655c95a4e7ce7d2aa8f93
Nelson-5553 <andresnelson2004@hotmail.com>
2025-09-04 15:32:50 -0500

    Refactor create company modal: enhance structure and improve user experience

--- resources/views/components/create-companies.blade.php
+++ resources/views/components/create-companies.blade.php
@@ -1,30 +1,21 @@
-<div>
 
-<!-- Modal toggle -->
-<div class="flex justify-end">
-<button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="block text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
-  Crear Empresa
-</button>
-</div>
-<!-- Main modal -->
-<div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow-sm">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
-                <h3 class="text-lg font-semibold text-gray-900">
-                    Crear Nueva Empresa
-                </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-toggle="crud-modal">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+<div x-data="{modalIsOpen: false}">
+    <button x-on:click="modalIsOpen = true" type="button" class="whitespace-nowrap rounded-sm bg-purple-700 border border-purple-700 px-4 py-2 text-center text-sm font-medium tracking-wide text-white transition hover:opacity-75 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-700 active:opacity-100 active:outline-offset-0">Crear Empresa</button>
+    <div x-cloak x-show="modalIsOpen" x-transition.opacity.duration.200ms x-trap.inert.noscroll="modalIsOpen" x-on:keydown.esc.window="modalIsOpen = false" x-on:click.self="modalIsOpen = false" class="fixed inset-0 z-30 flex items-end justify-center bg-black/20 p-4 pb-8 backdrop-blur-md sm:items-center lg:p-8" role="dialog" aria-modal="true" aria-labelledby="defaultModalTitle">
+        <!-- Modal Dialog -->
+    <div x-show="modalIsOpen" x-transition:enter="transition ease-out duration-200 delay-100 motion-reduce:transition-opacity w-full" x-transition:enter-start="opacity-0 scale-50" x-transition:enter-end="opacity-100 scale-100" class="flex w-full max-w-2xl flex-col gap-4 overflow-hidden rounded-sm border border-neutral-300 bg-white text-neutral-600">
+            <!-- Dialog Header -->
+            <div class="flex items-center justify-between border-b border-neutral-300 bg-neutral-50/60 p-4 w-full">
+                <h3 id="defaultModalTitle" class="font-semibold tracking-wide text-neutral-900">Crear Nueva Empresa</h3>
+                <button x-on:click="modalIsOpen = false" aria-label="close modal">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" stroke="currentColor" fill="none" stroke-width="1.4" class="w-5 h-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
                     </svg>
-                    <span class="sr-only">Close modal</span>
                 </button>
             </div>
-            <!-- Modal body -->
-            <form action="{{ route('empresas.store') }}" method="POST" class="p-4 md:p-5">
+            <div class="w-full">
+            <!-- Dialog Body -->
+            <form action="{{ route('empresas.store') }}" method="POST" class="px-4 py-8">
                 @csrf
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <div class="col-span-2">
@@ -40,13 +31,14 @@
                         <input type="number" name="telefono" id="telefono" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5" placeholder="Teléfono de la empresa" required>
                     </div>
                 </div>
-                <button type="submit" class="text-white inline-flex items-center bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+
+                <!-- Dialog Footer -->
+                <button type="submit" class="text-white inline-flex items-center bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium text-sm px-5 py-2.5 text-center">
                     <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                     Agregar Empresa
                 </button>
             </form>
         </div>
+        </div>
     </div>
-</div>
-
 </div>
